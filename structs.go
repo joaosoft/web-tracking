7139e7262f337e4f7b190cafd476c163736669d6
João Ribeiro <joao.ribeiro@foursource.pt>
2019-07-01 19:07:56 +0100

    fix

--- structs.go
+++ structs.go
@@ -8,6 +8,8 @@ type TrackingRequest struct {
 		Action    *string          `json:"action" validate:"notzero"`
 		Label     *string          `json:"label"`
 		Value     *int64           `json:"value"`
+		Viewer    *string          `json:"viewer"`
+		Viewed    *string          `json:"viewed"`
 		Latitude  *float64         `json:"latitude"`
 		Longitude *float64         `json:"longitude"`
 		Street    *string          `json:"street"`
